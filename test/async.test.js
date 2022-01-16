ec4c41941eed60cdb35464268516b7d1e897c70e
Daniel Buckmaster <daniel@crabmusket.net>
2022-01-15 22:12:12 -0800

    Format code using Deno

--- test/async.test.js
+++ test/async.test.js
@@ -1,23 +1,17 @@
-import {performance} from 'perf_hooks';
+import { performance } from "perf_hooks";
 globalThis.performance = performance;
 
-import benchmark from '../index.js';
-import test from 'tape';
+import benchmark from "../index.js";
+import test from "tape";
 
-test('[async] benchmark a simple function', async t => {
-  const result = await benchmark(function(done) { for (let i = 0; i < 1000; i++) {} done(); }, undefined, 10);
-  t.ok(result.count > 0);
-  t.ok(result.mean >= 0);
-  t.ok(result.max >= 0);
-  t.ok(result.min >= 0);
-});
-
-test('[async] benchmark a function with setup', async t => {
-  let iters = 0;
+test("[async] benchmark a simple function", async (t) => {
   const result = await benchmark(
-    function(done) { for (let i = 0; i < iters; i++) {} done(); },
-    function() { iters = 500 + Math.random() * 1000; },
-    10
+    function (done) {
+      for (let i = 0; i < 1000; i++) {}
+      done();
+    },
+    undefined,
+    10,
   );
   t.ok(result.count > 0);
   t.ok(result.mean >= 0);
@@ -25,8 +19,29 @@ test('[async] benchmark a function with setup', async t => {
   t.ok(result.min >= 0);
 });
 
-test('[async] duration changes the number of samples', async t => {
-  const func = function(done) { for (let i = 0; i < 1000; i++) {} done(); };
+test("[async] benchmark a function with setup", async (t) => {
+  let iters = 0;
+  const result = await benchmark(
+    function (done) {
+      for (let i = 0; i < iters; i++) {}
+      done();
+    },
+    function () {
+      iters = 500 + Math.random() * 1000;
+    },
+    10,
+  );
+  t.ok(result.count > 0);
+  t.ok(result.mean >= 0);
+  t.ok(result.max >= 0);
+  t.ok(result.min >= 0);
+});
+
+test("[async] duration changes the number of samples", async (t) => {
+  const func = function (done) {
+    for (let i = 0; i < 1000; i++) {}
+    done();
+  };
   const result = await benchmark(func, undefined, 100);
   const result2 = await benchmark(func, undefined, 300);
   t.ok(result2.count > result.count);
--- test/sync.test.js
+++ test/sync.test.js
@@ -1,24 +1,16 @@
-import {performance} from 'perf_hooks';
+import { performance } from "perf_hooks";
 globalThis.performance = performance;
 
-import benchmark from '../index.js';
-import test from 'tape';
+import benchmark from "../index.js";
+import test from "tape";
 
-test('[sync] benchmark a simple function', t => {
-  const result = benchmark(function() { for (let i = 0; i < 1000; i++) {} }, undefined, 10);
-  t.ok(result.count > 0);
-  t.ok(result.mean >= 0);
-  t.ok(result.max >= 0);
-  t.ok(result.min >= 0);
-  t.end();
-});
-
-test('[sync] benchmark a function with setup', t => {
-  let iters = 0;
+test("[sync] benchmark a simple function", (t) => {
   const result = benchmark(
-    function() { for (let i = 0; i < iters; i++) {} },
-    function() { iters = 500 + Math.random() * 1000; },
-    10
+    function () {
+      for (let i = 0; i < 1000; i++) {}
+    },
+    undefined,
+    10,
   );
   t.ok(result.count > 0);
   t.ok(result.mean >= 0);
@@ -27,8 +19,28 @@ test('[sync] benchmark a function with setup', t => {
   t.end();
 });
 
-test('[sync] duration changes the number of samples', t => {
-  const func = function() { for (let i = 0; i < 1000; i++) {} };
+test("[sync] benchmark a function with setup", (t) => {
+  let iters = 0;
+  const result = benchmark(
+    function () {
+      for (let i = 0; i < iters; i++) {}
+    },
+    function () {
+      iters = 500 + Math.random() * 1000;
+    },
+    10,
+  );
+  t.ok(result.count > 0);
+  t.ok(result.mean >= 0);
+  t.ok(result.max >= 0);
+  t.ok(result.min >= 0);
+  t.end();
+});
+
+test("[sync] duration changes the number of samples", (t) => {
+  const func = function () {
+    for (let i = 0; i < 1000; i++) {}
+  };
   const result = benchmark(func, undefined, 100);
   const result2 = benchmark(func, undefined, 300);
   t.ok(result2.count > result.count);
