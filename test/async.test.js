e219668420c660f6f9ef9a84faece59ae2a21d7e
Daniel Buckmaster <daniel@crabmusket.net>
2022-01-15 22:11:32 -0800

    Use const instead of let

--- test/async.test.js
+++ test/async.test.js
@@ -5,7 +5,7 @@ import benchmark from '../index.js';
 import test from 'tape';
 
 test('[async] benchmark a simple function', async t => {
-  let result = await benchmark(function(done) { for (let i = 0; i < 1000; i++) {} done(); }, undefined, 10);
+  const result = await benchmark(function(done) { for (let i = 0; i < 1000; i++) {} done(); }, undefined, 10);
   t.ok(result.count > 0);
   t.ok(result.mean >= 0);
   t.ok(result.max >= 0);
@@ -14,7 +14,7 @@ test('[async] benchmark a simple function', async t => {
 
 test('[async] benchmark a function with setup', async t => {
   let iters = 0;
-  let result = await benchmark(
+  const result = await benchmark(
     function(done) { for (let i = 0; i < iters; i++) {} done(); },
     function() { iters = 500 + Math.random() * 1000; },
     10
@@ -26,8 +26,8 @@ test('[async] benchmark a function with setup', async t => {
 });
 
 test('[async] duration changes the number of samples', async t => {
-  let func = function(done) { for (let i = 0; i < 1000; i++) {} done(); };
-  let result = await benchmark(func, undefined, 100);
-  let result2 = await benchmark(func, undefined, 300);
+  const func = function(done) { for (let i = 0; i < 1000; i++) {} done(); };
+  const result = await benchmark(func, undefined, 100);
+  const result2 = await benchmark(func, undefined, 300);
   t.ok(result2.count > result.count);
 });
--- test/sync.test.js
+++ test/sync.test.js
@@ -5,7 +5,7 @@ import benchmark from '../index.js';
 import test from 'tape';
 
 test('[sync] benchmark a simple function', t => {
-  let result = benchmark(function() { for (let i = 0; i < 1000; i++) {} }, undefined, 10);
+  const result = benchmark(function() { for (let i = 0; i < 1000; i++) {} }, undefined, 10);
   t.ok(result.count > 0);
   t.ok(result.mean >= 0);
   t.ok(result.max >= 0);
@@ -15,7 +15,7 @@ test('[sync] benchmark a simple function', t => {
 
 test('[sync] benchmark a function with setup', t => {
   let iters = 0;
-  let result = benchmark(
+  const result = benchmark(
     function() { for (let i = 0; i < iters; i++) {} },
     function() { iters = 500 + Math.random() * 1000; },
     10
@@ -28,9 +28,9 @@ test('[sync] benchmark a function with setup', t => {
 });
 
 test('[sync] duration changes the number of samples', t => {
-  let func = function() { for (let i = 0; i < 1000; i++) {} };
-  let result = benchmark(func, undefined, 100);
-  let result2 = benchmark(func, undefined, 300);
+  const func = function() { for (let i = 0; i < 1000; i++) {} };
+  const result = benchmark(func, undefined, 100);
+  const result2 = benchmark(func, undefined, 300);
   t.ok(result2.count > result.count);
   t.end();
 });
